6a4b8b6dbdac8c2f44a0007ca84d8a6214596c76
SuperBluestar <alexander116gm@gmail.com>
2022-04-19 18:07:26 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="text-6xl">React Typescript Prettier Eslint Husky</header>
+    </div>
+  );
+}
+
+export default App;

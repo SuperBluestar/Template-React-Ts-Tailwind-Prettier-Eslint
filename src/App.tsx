2314c17df5c0336a124c2fcc3d25062b0c88468d
SuperBluestar <alexander116gm@gmail.com>
2022-04-25 08:51:48 +0900

    complete react router v6

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="text-6xl">React Typescript Prettier Eslint Husky</header>
-    </div>
-  );
-}
-
-export default App;
